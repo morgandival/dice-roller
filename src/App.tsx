c34d0e768eb323bf9c8c9406859e8307928d8f82
Morgan Di Val <morgandival@gmail.com>
2021-03-09 01:06:06 +1100

    Built basic form

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import Dice from './components/Dice'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <Dice />
     </div>
   );
 }
