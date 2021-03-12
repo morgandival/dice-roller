db8dbb99ee01f8537c880da8b1505714fdd6e55d
Morgan Di Val <morgandival@gmail.com>
2021-03-12 15:56:55 +1100

    More prettier code

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import Dice from './components/Dice';
-import './App.css';
+import React from "react";
+import Dice from "./components/Dice";
+import "./App.css";
 
 function App() {
   return (
