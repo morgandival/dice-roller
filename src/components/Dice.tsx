9fb2b72265a8ecaa042a62ddb504334e6353b4af
Morgan Di Val <morgandival@gmail.com>
2021-03-12 15:51:19 +1100

    Prettified code

--- src/components/Dice.tsx
+++ src/components/Dice.tsx
@@ -1,19 +1,17 @@
 import React from 'react';
 
-
-function Dice () {
-
-  return(
+function Dice() {
+  return (
     <div className="dice">
       <form>
         <select>
-            <option>d20</option>
-            <option>d12</option>
-            <option>d10</option>
-            <option>d8</option>
-            <option>d6</option>
-            <option>d4</option>
-            <option>d100</option>
+          <option>d20</option>
+          <option>d12</option>
+          <option>d10</option>
+          <option>d8</option>
+          <option>d6</option>
+          <option>d4</option>
+          <option>d100</option>
         </select>
         <button>Roll!</button>
       </form>
