f050c84983065989ba91e38a1ca7b02f0958c9bd
Morgan Di Val <morgandival@gmail.com>
2021-03-16 14:02:25 +1100

    Added event handler logic for dice selection and form submission, added styles

--- src/components/Dice.tsx
+++ src/components/Dice.tsx
@@ -1,20 +1,44 @@
 import React from "react";
+import "./Dice.css";
 
 function Dice() {
+  // Declare states for dice and result
+  const [dice, setDice] = React.useState(20);
+  const [random, setRandom] = React.useState(0);
+
+  // Handles what happens when the Roll button is clicked
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    // prevent default submit action
+    event.preventDefault();
+    // roll the dice
+    setRandom(Math.floor(Math.random() * dice) + 1);
+  };
+
+  // Handles what happens when the select option is changed
+  const handleSelect = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    // get value of select option and parse as integer
+    let x = parseInt(event.target.value);
+    // set value of dice
+    setDice(x);
+  };
+
   return (
     <div className="dice">
-      <form>
-        <select>
-          <option>d20</option>
-          <option>d12</option>
-          <option>d10</option>
-          <option>d8</option>
-          <option>d6</option>
-          <option>d4</option>
-          <option>d100</option>
+      <form onSubmit={handleSubmit}>
+        <select onChange={handleSelect}>
+          <option value="20">d20</option>
+          <option value="12">d12</option>
+          <option value="10">d10</option>
+          <option value="8">d8</option>
+          <option value="6">d6</option>
+          <option value="4">d4</option>
+          <option value="100">d100</option>
         </select>
         <button>Roll!</button>
       </form>
+      <p>
+        You rolled: <span className="result">{random}</span>
+      </p>
     </div>
   );
 }
