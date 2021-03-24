86f26a2b2e5098fb114224c3d5dd80fd74a4740d
Morgan Di Val <morgandival@gmail.com>
2021-03-25 01:55:27 +1100

    Added roll history functionality

--- src/components/Dice.tsx
+++ src/components/Dice.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import './Dice.css';
 
+// Define what a dice roll should look like to the history array
+interface Roll {
+  random: number;
+  dice: number;
+}
+
 function Dice() {
   // Declare states for dice, random result, and previous rolls
   const [dice, setDice] = React.useState(20);
   const [random, setRandom] = React.useState(0);
-  const [history, setHistory] = React.useState<object[]>([]);
+  const [history, setHistory] = React.useState<Roll[]>([]);
 
   // Handles what happens when the Roll button is clicked
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
@@ -17,22 +23,45 @@ function Dice() {
 
     // Add result to history array
     setHistory([{ random, dice }, ...history]);
-
-    console.log(`History items: ${history.length}`);
-
-    // Error checking
-    if (history.length > 0) {
-      console.log(history);
-    } else {
-      console.log(`No history items to display!`);
-    }
   };
 
   // Handles what happens when the select option is changed
   const handleSelect = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    // Push last roll to beginning of history array
+    setHistory([{ random, dice }, ...history]);
+
+    // Re-initialise random
+    setRandom(0);
+
     // Get value of select option, parse as integer, set dice value
     setDice(parseInt(event.target.value));
   };
+
+  // Handles what happens when reset button is clicked
+  const handleReset = (event: React.MouseEvent<HTMLButtonElement>) => {
+    // Reset random and history to default values
+    setRandom(0);
+    setHistory([]);
+
+    // Log click
+    console.log('Reset clicked');
+  };
+
+  // Validation logging - uncomment lines below to examine history array
+  // console.log(`History items: ${history.length}`);
+  // console.log(history);
+
+  // Initialise history output array
+  const output: Array<string> = [];
+
+  // Loop through roll history
+  history.forEach(function (roll) {
+    // Negate addition of initial zero rolls to output array
+    if (roll.random !== 0) {
+      // Add string to output array
+      output.push(`${roll.random} (${roll.dice})`);
+    }
+  });
 
   return (
     <div className="dice">
@@ -47,11 +76,28 @@ function Dice() {
           <option value="100">d100</option>
         </select>
         <button>Roll!</button>
+        <p>
+          You rolled:{' '}
+          <span className="result">
+            {random} ({dice})
+          </span>
+        </p>
       </form>
-      <p>
-        You rolled: <span className="result">{random}</span>
-      </p>
-      <h4>Roll History</h4>
+      <div className="results">
+        <div className="results-title">
+          <h4>Roll History</h4>
+        </div>
+        <div className="results-reset">
+          <button onClick={handleReset}>Clear</button>
+        </div>
+        <div className="results-items">
+          <ol>
+            {output.map((listItem) => (
+              <li>{listItem}</li>
+            ))}
+          </ol>
+        </div>
+      </div>
     </div>
   );
 }
