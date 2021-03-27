4c484c8629db7c1c9ad174308d884f6492b958a3
Morgan Di Val <morgandival@gmail.com>
2021-03-27 13:27:33 +1100

    Added header to project so that people can return to main site

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+  return (
+    <header>
+      <a href="/index.html">Back to morgandival.github.io</a>
+    </header>
+  );
+}
+
+export default Header;
